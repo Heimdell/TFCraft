15d045bb773f8ba174284d15777ae9573a2ef49a
Heimdell <hindmost.one@gmail.com>
2015-06-08 20:33:32 +0300

    Commented the code

--- src/Common/com/heimdell/utils/traversal/Traversal.java
+++ src/Common/com/heimdell/utils/traversal/Traversal.java
@@ -4,8 +4,24 @@ package com.heimdell.utils.traversal;
 import java.util.*;
 
 public abstract class Traversal {
-	protected abstract int     classify(Point point);
+	// assign the number to the block at given point
+	// it will act as a priority
+	protected abstract int classify(Point point);
+
+	// check, if we could traverse from one color to another
+	// for instance: 
+	//     YES for wood -> wood, 
+	//     YES for wood -> leaves, 
+	//     YES for leaves -> leaves,
+	//     NO  otherwise
+	//
+	// in dense forests it could wipe more leaves that supposed to;
+	// however, we could classify leaves farther than +/-5 X/Z as air
+	// to stop the algorithm
 	protected abstract boolean canGo   (int colorFrom, int colorTo);
+
+	// perform some action on traversed block:
+	//   cut the wood, wipe the leaves, update the air, etc.
 	protected abstract void    dispatch(Point point);
 
 	protected class Point implements Comparable<Point> {
@@ -17,6 +33,7 @@ public abstract class Traversal {
 			this.z = z;
 		}
 
+		// cache the classification
 		public void makeClassified() {
 			if (this.color == 0)
 				this.color = classify(this);
@@ -26,13 +43,26 @@ public abstract class Traversal {
 			return new Point(x + dx, y + dy, z + dz);
 		}
 
+		// make sure that PriorityQueue will pop higher-colored points first
 		@Override
 		public int compareTo(Point point) {
 			return point.color - color;
 		}
+
+		// ignore the color on comparison (we compare locations here)
+		@Override
+		public boolean equals(Object object) {
+			if (object instanceof Point) {
+				Point point = (Point) object;
+
+				return x == point.x && y == point.y && z == point.z;
+			}
+			return false;
+		}
 	}
 
-	protected final int NO_ENTER = 0;
+	// the "air" constant
+	protected final int AIR = 0;
 
 	private Vector<Point> vicinity(Point point) {
 		Vector<Point>
@@ -52,6 +82,7 @@ public abstract class Traversal {
 		Queue<Point>
 			queue   = new PriorityQueue();
 
+		// we will return this container
 		HashSet<Point>
 			visited = new HashSet();
 
@@ -63,8 +94,9 @@ public abstract class Traversal {
 
 			visited.add(current);
 
-			if (current.color != NO_ENTER)
+			if (current.color != AIR)
 				for (Point near : vicinity(point)) {
+					// make sure we're no classifying same point twice
 					if (!visited.contains(near)) {
 						near.makeClassified();
 
