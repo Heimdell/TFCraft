febaf3924bdcf439f0ccf5f73663818f5f8af840
Heimdell <hindmost.one@gmail.com>
2015-06-08 20:39:46 +0300

    Added some more comments

--- src/Common/com/heimdell/utils/traversal/Traversal.java
+++ src/Common/com/heimdell/utils/traversal/Traversal.java
@@ -3,6 +3,16 @@ package com.heimdell.utils.traversal;
 
 import java.util.*;
 
+/*
+	This class is supposed to be anonimously overriden, implementing some
+	methods.
+
+	The "vicinity" method could be overriden, too, if the 26-vicinity 
+	doesn't fit your purposes.
+
+	Traversal traversal = new Traversal() { ... implementation... }
+	traversal.run();
+*/
 public abstract class Traversal {
 	// assign the number to the block at given point
 	// it will act as a priority
@@ -64,7 +74,7 @@ public abstract class Traversal {
 	// the "air" constant
 	protected final int AIR = 0;
 
-	private Vector<Point> vicinity(Point point) {
+	protected Vector<Point> vicinity(Point point) {
 		Vector<Point>
 			result = new Vector();
 
